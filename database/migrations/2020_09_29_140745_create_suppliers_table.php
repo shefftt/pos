8dc14d0a4d2563877a413337c1a57d2058b1a8d3
shefftt <ahmed7med@gmail.com>
2020-10-05 14:52:58 +0200

    edit supplier table

--- database/migrations/2020_09_29_140745_create_suppliers_table.php
+++ database/migrations/2020_09_29_140745_create_suppliers_table.php
@@ -17,6 +17,7 @@ class CreateSuppliersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone');
+            $table->string('status')->default(1);
             $table->string('address');
             $table->timestamps();
         });
